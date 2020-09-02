84e460373482b0a87f4e8e374f9691e9ddecb597
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-09-02 18:20:08 -0500

    Updates

---
+++ precache-manifest.3c13f2e2c8d1ded08ba977d545ff5df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f12d56670c189401863b46d557b9aaf",
+    "url": "/CVReact/index.html"
+  },
+  {
+    "revision": "5f33cbedf2a2dd6f90b6",
+    "url": "/CVReact/static/css/main.f503dad3.chunk.css"
+  },
+  {
+    "revision": "5c9de54582e37675fba8",
+    "url": "/CVReact/static/js/2.b0162cf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CVReact/static/js/2.b0162cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f33cbedf2a2dd6f90b6",
+    "url": "/CVReact/static/js/main.6fb26ff5.chunk.js"
+  },
+  {
+    "revision": "1d9932826b9a0320e76b",
+    "url": "/CVReact/static/js/runtime-main.eb380d55.js"
+  }
+]);
